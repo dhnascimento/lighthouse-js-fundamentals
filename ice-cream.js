17b482dca61651a4d65b0b0f3e9fa4b0b507dffe
Daniel Nascimento <danielhn@gmail.com>
2020-07-07 01:58:45 +0000

    Ice Cream Prep Work solution.

---
+++ ice-cream.js
@@ -0,0 +1,17 @@
+// Do not modify these first two lines
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+// Do not modify these first two lines
+
+iceCreamFlavours.push("root beer");
+
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]);
+
+console.log(iceCreamFlavours[5]);
+
+console.log(iceCreamFlavours.length);
+
